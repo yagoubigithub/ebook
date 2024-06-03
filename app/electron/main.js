1b881411fe446c66f7c6fa35dfa5ff30b2913252
abdelkader <yagoubi.aek.2@gmail.com>
2024-06-03 09:42:24 +0100

    create windows folder

--- app/electron/main.js
+++ app/electron/main.js
@@ -1,39 +1,20 @@
-import { app, BrowserWindow } from "electron";
-const isDevelopment = process.env.NODE_ENV === "development";
+import { app, BrowserWindow } from 'electron';
 
-function createWindow() {
-  // Create a new window
-  const window = new BrowserWindow({
-    width: 800,
-    height: 600,
-    show: false,
-  });
-
-  // Event listeners on the window
-  window.webContents.on("did-finish-load", () => {
-    window.show();
-    window.focus();
-  });
-
-  // Load our HTML file
-  if (isDevelopment) {
-    window.loadURL("http://localhost:40992");
-  } else {
-    console.log("prod");
-    window.loadFile("app/dist/index.html");
-  }
-}
+import createWindow from './windows/main/index.js';
 
 // This method is called when Electron
 // has finished initializing
-app.whenReady().then(() => {
-  createWindow();
 
-  app.on("activate", () => {
+let mainWindow;
+app.whenReady().then(() => {
+  mainWindow = createWindow();
+  mainWindow.show();
+
+  app.on('activate', () => {
     // On macOS it's common to re-create a window in the app when the
     // dock icon is clicked and there are no other windows open.
     if (BrowserWindow.getAllWindows().length === 0) {
-      createWindow();
+      mainWindow = createWindow();
     }
   });
 });
@@ -41,8 +22,8 @@ app.whenReady().then(() => {
 // Quit when all windows are closed, except on macOS. There, it's common
 // for applications and their menu bar to stay active until the user quits
 // explicitly with Cmd + Q.
-app.on("window-all-closed", function () {
-  if (process.platform !== "darwin") {
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
     app.quit();
   }
 });
---
+++ app/electron/windows/main/index.js
@@ -0,0 +1,38 @@
+import { BrowserWindow, app } from 'electron';
+
+import path from 'path';
+import __dirname from '../../__dirname.js';
+
+export default function createWindow() {
+  // our window you can chanege the size  and other
+
+  const window = new BrowserWindow({
+    show: true,
+    height: 500,
+    width: 800,
+
+    webPreferences: {
+      preload: path.join(__dirname, './preload.js'),
+      nodeIntegration: true,
+    },
+  });
+
+  if (app.isPackaged) {
+    window.loadFile('./app/dist/index.html');
+
+    // window.setMenu(null);
+  } else {
+    window.loadURL('http://localhost:40992');
+  }
+
+  // Automatically open Chrome's DevTools in development mode.
+  if (!app.isPackaged) {
+    // window.webContents.openDevTools();
+  }
+
+  window.on('close', () => {
+    app.quit();
+    app.exit(0);
+  });
+  return window;
+}
