e3d7a1511bc068d5d5ffff748e3af0c56990d7d7
abdelkader <yagoubi.aek.2@gmail.com>
2024-06-03 09:22:50 +0100

    lint

--- app/src/index.jsx
+++ app/src/index.jsx
@@ -1,9 +1,9 @@
-import React from "react";
-import { createRoot } from "react-dom/client";
+import React from 'react';
+import { createRoot } from 'react-dom/client';
 
-import "./index.scss";
+import './index.scss';
 
-const container = document.getElementById("target");
+const container = document.getElementById('target');
 const root = createRoot(container);
 
-root.render(<React.Fragment>Hello world from React</React.Fragment>);
+root.render(<>Hello world from React</>);
